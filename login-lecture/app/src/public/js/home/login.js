a8763e6069b59cb9d9440a350d94e48e49f55378
quar16 <90584642+junghyonil123@users.noreply.github.com>
2023-02-10 17:28:09 +0900

    Init

---
+++ login-lecture/app/src/public/js/home/login.js
@@ -0,0 +1,29 @@
+"use strict"
+
+console.log("hello");
+
+const id = document.querySelector("#id"),
+    password = document.querySelector("#password"),
+    loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login(){
+
+    console.log("login");
+    const req = {
+        id: id.value,
+        password: password.value,
+    };
+    
+    console.log(JSON.stringify(req));
+
+    fetch("/login",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req),
+    });
+
+}
---
+++ login-lecture/app/src/routes/home/home.ctrl.js
@@ -0,0 +1,26 @@
+"use strict";
+
+
+const output = {
+    home : (req, res) =>{
+        res.render("home/index");
+    },
+    
+    
+    login : (req, res) => {
+        res.render("home/login")
+    },
+}
+
+
+const process = {
+    login: (req, res) => {
+        console.log(req.body);
+    },
+};
+
+
+module.exports = {
+    output,
+    process,
+};
